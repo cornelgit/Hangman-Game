001b4e36f56727ab1e5497cc67d88edf29864783
Cornel Stoica <cornel.stoica@oit.edu>
2020-01-07 01:10:40 -0800

    Push #3 (again).

--- Hangman Game/Hangman Game.cpp
+++ Hangman Game/Hangman Game.cpp
@@ -1,5 +1,5 @@
 //A hangman game with multiple difficulties and a scoring system.
-//Avaialble modes (single player only).
+//Available modes (single player only).
 //
 //Created by: Cornel Stoica
 //Date: 1/1/20
@@ -9,6 +9,7 @@
 #include <fstream>
 #include <string>
 #include <ctime>
+#include <algorithm>
 
 using namespace std;
 
@@ -18,8 +19,11 @@ int get_option();
 int get_difficulty();
 string open_file(ifstream &);
 string get_word(ifstream &, int);
+void start_guess(string, string &, string &, int);
+void display_unguessed(string);
 int line_count(ifstream &, string);
 int random_line(int);
+bool check_word_guess(string word);
 
 int main()
 {
@@ -29,6 +33,8 @@ int main()
     int difficulty = { 0 };
     string file = { " " };
     string word = { " " };
+    string subWord = { " " };
+    string guessedChars = { " " };
     int count = { 0 };
     int randomLine = { 0 };
     
@@ -50,17 +56,29 @@ int main()
     
     file = open_file(input);
 
-    //get line (word) count in file
+    //get file line (word) count
     count = line_count(input, file);
 
     //get random line number from file
     randomLine = random_line(count);
 
-    //read the random line (get word)
+    //read the random line (word)
     word = get_word(input, randomLine);
 
+    word = "CORNEL"; //for testing purposes   ////////////////////////////////////////////////////////////////////////
 
+    subWord.resize(word.length()); //resize subWord variable to match word length
 
+    //reset subWord string to all underscores
+    for (unsigned int i = 0; i < subWord.length(); ++i) {
+        subWord[i] = '_';
+    }
+
+    //display unguessed word
+    display_unguessed(word);   
+        
+    //begin guessing game
+    start_guess(word, subWord, guessedChars, difficulty);
 
     cout << endl << endl << endl; //extra space at the end
 
@@ -130,20 +148,21 @@ string open_file(ifstream & input)
     string file = { "words.txt" };
     
     //attempt to open file containing words
+    cout << "\nAttempting to open \"" << file << "\".";
     input.open(file);
 
     //check if open is succesful
     while (!input)
     {
         cout << "\nCould not open \"" << file << "\". " << "Please try again." << endl;
-        cout << "Please enter the input filename including extension: ";
+        cout << "Please manually enter the input filename including extension (absolute path is also acceptable): ";
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         getline(cin, file);
         input.open(file); //attempt to open new filename
 
     }
 
-    cout << "Succesfuly opened \"" << file << "\"." << endl << endl; //if file was valid and opened
+    cout << "\nSuccesfuly opened filename \"" << file << "\"." << endl << endl; //if file was valid and opened
 
     return file;
 }
@@ -161,7 +180,7 @@ int line_count(ifstream & input, string file)
     }
     
     input.seekg(0); // rewind stream
-    cout << "Count is: " << count << endl;
+
     return count;
 }
 
@@ -191,6 +210,167 @@ string get_word(ifstream & input, int randomLine)
         else
             input.ignore(numeric_limits<streamsize>::max(), '\n'); //ignore words we don't need
     }
-
+    
+    //convert word to uppercase
+    transform(word.begin(), word.end(), word.begin(), ::toupper);
+  
     return word;
+}
+
+//Function to display word to be guessed
+void display_unguessed(string word)
+{
+    //display tip
+    cout << "\nNOTE: For each incorrect guess you are deducted a life. There is no penalty for attempting to guess full word." << endl;
+    
+    cout << "The word is " << word.length() << " characters long." << endl;
+    cout << "Word: ";
+
+    for (unsigned int i = 0; i < word.length(); ++i) {
+        cout << " _ ";
+    }   
+}
+
+//Function to start guessing word/letters
+void start_guess(string word, string & subWord, string & guessedChars, int difficulty)
+{
+    char guessChar = ' ';
+    bool guessedCorrectly = false;
+    bool loopFunction = false;
+    char tryAgain = { ' ' };
+        
+    //display remaining lives (guesses)
+    cout << "\tLives remaining [" << difficulty << "]" << endl;
+
+    //get guessing option
+    do {
+        cout << "\nPlease enter a guessing letter or enter (1) to try and guess full word. ";      
+        cout << "\nOtherwise press (0) to quit to main menu: ";      
+        cin >> guessChar;
+                       
+        if (cin.fail() || (!isalpha(guessChar) && guessChar != '1')) {
+                cout << "Not a valid selection. Please try again." << endl;
+                cin.clear();
+                cin.ignore();            
+        }
+
+        else if (isalpha(guessChar))
+            guessChar = toupper(guessChar); //convert guess to uppercase
+
+    } while (!isalpha(guessChar) && guessChar != '1' && guessChar != '0');
+
+    //if user wants to go back to main menu
+    if (guessChar == '0') {
+        cout << endl << endl;
+        main(); //restart from
+    }
+
+    //if user wants to guess full word
+    else if (guessChar == '1') {
+
+        //keep checking and asking for word if user wishes to do so
+        do {
+            guessedCorrectly = check_word_guess(word);
+            
+            cout << "Try again? Yes (Y) or No (N): ";
+            cin >> tryAgain;
+
+            if (cin.fail() || (!isalpha(tryAgain) && tryAgain != 'Y' && tryAgain != 'N')) {
+                cout << "Not a valid selection. Please try again. " << endl;
+                cin.clear();
+                cin.ignore();
+            }          
+
+            tryAgain = toupper(tryAgain);
+
+        } while (tryAgain == 'Y' && guessedCorrectly == false);
+    }
+
+    //if a valid letter was entered
+    else {
+        if (word.find(guessChar) != string::npos) {                   
+            if (guessedChars.find(guessChar) != string::npos)
+                cout << "\nYou've already guessed that letter." << endl;
+
+            else {
+                cout << "\nLetter found." << endl;
+                guessedChars.push_back(guessChar); //add guessed word to list if not already on it
+              
+                for (unsigned int i = 0; i < word.length(); ++i) {                   
+                    if (word[i] == guessChar && subWord[i] == '_')
+                            subWord[i] = guessChar;                 
+                }
+            }                                      
+        }        
+                       
+        else {
+            cout << "\nLetter not found." << endl;
+            guessedChars.push_back(guessChar); //add guessed word
+            --difficulty;                     
+        }
+        loopFunction = true;
+    }
+
+    //display partial word and guesses so far
+    cout << "Word: ";
+    for (unsigned int i = 0; i < word.length(); ++i) {
+        cout << subWord[i] << " ";
+    }
+    cout << "\tGuessed so far:";
+    for (unsigned int i = 0; i < guessedChars.length(); ++i) {
+        cout << guessedChars[i] << " ";
+    }
+
+    //if we want to loop through function again
+    if (loopFunction == true && guessedCorrectly == false && difficulty > 0)
+        start_guess(word, subWord, guessedChars, difficulty);
+
+    //if user completed word or ran out of lives
+    else {
+        if (difficulty == 0) {
+            cout << "Game over. You are out of lives. " << endl;
+            cout << "\nThe word is \"" << word << "\"." << endl << endl;
+            cout << "Returning to main menu." << endl;
+            main();
+        }
+
+        //if word is completed or guessed correctly
+        else {
+            cout << "\nThe word is \"" << word << "\"." << endl << endl;
+            cout << "Returning to main menu." << endl << endl << endl;
+            main();
+        }
+    }
+}
+
+//Function to check full word guess
+bool check_word_guess(string word)
+{
+    string wordGuess = { " " };
+    bool guessedCorrectly = false;
+     
+    //get user guessed word
+    cout << "\nWhat is the word? ";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin, wordGuess);
+    
+    //convert guessed word to uppercase
+    transform(wordGuess.begin(), wordGuess.end(), wordGuess.begin(), ::toupper);
+
+    //check if guessed word is correct and of right length
+    if (wordGuess.length() < word.length())
+        cout << "Your guess is too short. ";
+    else if (wordGuess.length() > word.length())
+        cout << "Your guess is too long. ";
+    else
+        if (wordGuess.length() == word.length()) {
+            if (wordGuess == word) {
+                guessedCorrectly = true;
+                cout << "You guessed correctly. ";
+            }
+            else
+                cout << "Sorry, that is not the correct word. ";
+        }
+             
+    return guessedCorrectly;
 }
